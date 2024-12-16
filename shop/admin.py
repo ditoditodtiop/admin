22c14f3418f84ef21205306a42624c71f5c9ea6f
narma1 <dito.narama@gmail.com>
2024-12-16 21:03:15 +0400

    first commit

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from .models import Category, Item, Tag, Image
 
-# Register your models here.
+class ImageInline(GenericTabularInline):
+    model = Image
+    extra = 1
+
+class ItemInline(admin.TabularInline):
+    model = Item
+    extra = 1
+    fields = ('name', 'price', 'description')
+    readonly_fields = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    ordering = ('name',)
+    inlines = [ImageInline, ItemInline]
+
+class TagInline(admin.StackedInline):
+    model = Item.tags.through
+    extra = 1
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'description')
+    search_fields = ('name', 'description')
+    ordering = ('-price',)
+    inlines = [ImageInline, TagInline]
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Tag, TagAdmin)
--- shop/models.py
+++ shop/models.py
@@ -3,23 +3,11 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.db.models import Count
 
-class CategoryManager(models.Manager):
-    def with_item_count(self):
-        return self.annotate(item_count=Count('items'))
-    
-class ItemManager(models.Manager):
-    def with_tag_count(self):
-        return self.annotate(tags_count=Count('tags'))
-    
-class TagManager(models.Manager):
-    def popular_tags(self, min_items):
-        return self.annotate(item_count=Count('items')).filter(item_count__gte=min_items)
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     images = GenericRelation('Image', related_query_name="category")
-    objects = CategoryManager()
 
     def __str__(self):
         return self.name
@@ -30,7 +18,6 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
     images = GenericRelation("Image", related_query_name="item")
-    objects = ItemManager()
 
     def __str__(self):
         return self.name
@@ -38,7 +25,6 @@ class Item(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     items = models.ManyToManyField(Item, related_name='tags', blank=True)
-    objects = TagManager()
 
     def __str__(self):
         return self.name
